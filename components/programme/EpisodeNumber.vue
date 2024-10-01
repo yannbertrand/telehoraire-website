9c8fa42edeba2144948e3d1bcb4bd4fd760cc495
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-10-01 16:11:57 +0200

    🔥 Remove episode number xmltv_ns format because data is mostly wrong

--- components/programme/EpisodeNumber.vue
+++ components/programme/EpisodeNumber.vue
@@ -8,40 +8,14 @@ const props = defineProps<{
 
 function getEpisodeNumber(episodeNum) {
   if (episodeNum && episodeNum.length > 0) {
-    const xmltvNsEpisodeNum = episodeNum.find(
-      ({ system }) => system === 'xmltv_ns'
-    );
     const onscreenEpisodeNum = episodeNum.find(
       ({ system }) => system === 'onscreen'
     );
 
-    if (xmltvNsEpisodeNum !== undefined) {
-      return formatEpisodeNumberNs(xmltvNsEpisodeNum._value);
-    } else if (onscreenEpisodeNum !== undefined) {
+    if (onscreenEpisodeNum !== undefined) {
       return onscreenEpisodeNum._value;
     }
   }
-}
-
-function formatEpisodeNumberNs(xmltvNsEpisodeNumber) {
-  const parts = xmltvNsEpisodeNumber.split('.');
-  if (parts.length !== 3) {
-    return undefined;
-  }
-
-  const seasonNumber = `${
-    Number.parseInt(parts[0].split('/')[0]) + 1
-  }`.padStart(2, '0');
-  const episodeNumber = `${
-    Number.parseInt(parts[1].split('/')[0]) + 1
-  }`.padStart(2, '0');
-
-  let result = `S${seasonNumber}E${episodeNumber}`;
-  if (parts[2].length > 0) {
-    result += ` (${parts[2]})`;
-  }
-
-  return result;
 }
 
 const episodeNumber = getEpisodeNumber(props.episodeNum);
