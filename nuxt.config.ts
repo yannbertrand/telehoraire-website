fcd1b7f78c7888148094217426e47e3fd2ff5594
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-10-05 14:23:33 +0200

    ✨ Add app title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: { baseURL: '/telehoraire-website/' },
-  css: ['@picocss/pico/css/pico.css', '@picocss/pico/css/pico.colors.css'],
-  modules: ['@nuxt/image'],
+	app: {
+		baseURL: "/telehoraire-website/",
+		head: {
+			title: "Téléhoraire",
+		},
+	},
+	css: ["@picocss/pico/css/pico.css", "@picocss/pico/css/pico.colors.css"],
+	modules: ["@nuxt/image"],
 
-  image: {
-    domains: ['img.bouygtel.fr'],
-  },
+	image: {
+		domains: ["img.bouygtel.fr"],
+	},
 
-  experimental: {
-    componentIslands: true,
-  },
+	experimental: {
+		componentIslands: true,
+	},
 
-  devtools: { enabled: true },
-  compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	compatibilityDate: "2024-04-03",
 });
