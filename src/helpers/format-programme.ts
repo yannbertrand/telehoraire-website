57c1d345f02eb22b642922d8571ba0244dd2811d
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-03-10 22:01:06 +0100

    🏗️ Switch to Astro

---
+++ src/helpers/format-programme.ts
@@ -0,0 +1,23 @@
+import type { Programme } from "../types.ts";
+import { formatChannelId } from "./format-channel-id.ts";
+
+export function formatProgramme(programme: Programme) {
+	const $start = new Date(programme.start);
+	const $stop = new Date(programme.stop);
+	const dateTimeFormat = new Intl.DateTimeFormat("fr-FR", {
+		timeStyle: "short",
+	});
+
+	return {
+		channel: formatChannelId(programme.channel),
+		title: programme.title,
+		start: programme.start,
+		stop: programme.stop,
+		startStop: dateTimeFormat.formatRange($start, $stop),
+		desc: programme.desc,
+		category: programme.category,
+		subTitle: programme.subTitle,
+		icon: programme.icon,
+		episodeNum: programme.episodeNum,
+	};
+}
