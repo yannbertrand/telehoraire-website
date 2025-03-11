f312e1bbfe9205ae5cc9e0a2a0cb679632dedf1f
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-03-11 08:57:02 +0100

    ♻️ Use hash-sum to generate programmes hashes

--- src/helpers/get-programme-id.ts
+++ src/helpers/get-programme-id.ts
@@ -1,9 +1,6 @@
-import { createHash } from "node:crypto";
+import hashSum from "hash-sum";
 import type { Programme } from "../types.ts";
 
 export function getProgrammeId(programme: Programme) {
-	const hash = createHash("md5");
-
-	hash.update(JSON.stringify(programme));
-	return hash.copy().digest("hex");
+	return hashSum(programme);
 }
